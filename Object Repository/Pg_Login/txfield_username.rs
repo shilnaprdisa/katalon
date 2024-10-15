1e40e93e1ef1c8faf84aba52b2000e9518168e8a
Shilna Paradisa <shilnaparadisa6@gmail.com>
2024-10-15 10:57:00 +0700

    nambah object

---
+++ Object Repository/Pg_Login/txfield_username.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txfield_username</name>
+   <tag></tag>
+   <elementGuidId>c669b8d6-7bb2-4c76-81cf-5edbd6e78530</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//input[@id='username'])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
